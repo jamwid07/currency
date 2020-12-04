04136ae61ba6e278a1b26663195276a1bbd9e75b
Jamshid <jamwid07@mail.ru>
2020-12-05 00:40:36 +0500

    add currency console controller and parse action

---
+++ console/controllers/CurrencyController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Author: J.Namazov
+ * email:  <jamwid07@mail.ru>
+ * date:   05.12.2020
+ */
+
+namespace console\controllers;
+
+use yii\console\Controller;
+
+class CurrencyController extends Controller
+{
+    public function actionParse()
+    {
+        echo "Started parsing currencies\n";
+
+
+        echo "End parsing\n";
+    }
+}
