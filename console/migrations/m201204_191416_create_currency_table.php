14c8bcb183dab9d13f4fb6d14a0d23c2d794bb6e
Jamshid <jamwid07@mail.ru>
2020-12-05 00:50:07 +0500

    add currency code for search

--- console/migrations/m201204_191416_create_currency_table.php
+++ console/migrations/m201204_191416_create_currency_table.php
@@ -14,10 +14,13 @@ class m201204_191416_create_currency_table extends Migration
     {
         $this->createTable("{{%currency}}", [
             'id' => $this->primaryKey(),
+            'code' => $this->string(8),
             'name' => $this->string(255),
             'rate' => $this->float(),
             'insert_dt' => $this->dateTime(),
         ]);
+
+        $this->createIndex('idx-currency-code', "{{%currency}}", 'code');
     }
 
     /**
@@ -25,6 +28,7 @@ class m201204_191416_create_currency_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropIndex('idx-currency-code', "{{%currency}}");
         $this->dropTable("{{%currency}}");
     }
 }
