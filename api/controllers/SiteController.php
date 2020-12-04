0ec9d9d0c1512128b901d5ff4639d22265f71640
Jamshid <jamwid07@mail.ru>
2020-12-05 01:22:20 +0500

    add API actions

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -1,19 +1,12 @@
 <?php
 namespace api\controllers;
 
-use api\models\ResendVerificationEmailForm;
-use api\models\VerifyEmailForm;
-use Yii;
-use yii\base\InvalidArgumentException;
-use yii\web\BadRequestHttpException;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
-use api\models\PasswordResetRequestForm;
-use api\models\ResetPasswordForm;
-use api\models\SignupForm;
-use api\models\ContactForm;
+use common\models\Currency;
+use common\models\CurrencySearch;
+use yii\data\ActiveDataProvider;
+use yii\rest\Controller;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
 
 /**
  * Site controller
@@ -29,4 +22,31 @@ class SiteController extends Controller
         ];
     }
 
+    /**
+     * Returns list of currencies with pagination
+     * @return ActiveDataProvider
+     */
+    public function actionCurrencies(): ActiveDataProvider
+    {
+        $searchModel = new CurrencySearch();
+
+        return $searchModel->search(\Yii::$app->request->queryParams);
+    }
+
+    /**
+     * Returns found currency via its code
+     * @param $code string Standart charCode of currency
+     * @return Currency
+     * @throws NotFoundHttpException if currency code not found
+     */
+    public function actionCurrency(string $code): Currency
+    {
+        $currency = Currency::findOne(['code' => $code]);
+
+        if ($currency === null) {
+            throw new NotFoundHttpException("Currency not found");
+        }
+
+        return $currency;
+    }
 }
